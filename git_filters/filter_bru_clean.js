1c950224e9c9e4e81d08df6c98502c7f813adbef
Miguel A Valdés G <mvaldes988@gmail.com>
2025-03-29 14:49:44 -0600

    fix: restore all request (endpoints) files

--- git_filters/filter_bru_clean.js
+++ git_filters/filter_bru_clean.js
@@ -1,36 +1,36 @@
-function cleanSingle(obj) {
-  if (obj.meta && obj.meta.seq !== undefined) {
-    delete obj.meta.seq;
-  }
+// function cleanSingle(obj) {
+//   if (obj.meta && obj.meta.seq !== undefined) {
+//     delete obj.meta.seq;
+//   }
 
-  if (obj.request && obj.request.params && obj.request.params.path !== undefined) {
-    delete obj.request.params.path;
-  }
+//   if (obj.request && obj.request.params && obj.request.params.path !== undefined) {
+//     delete obj.request.params.path;
+//   }
 
-  return obj;
-}
+//   return obj;
+// }
 
-function cleanData(data) {
-  if (Array.isArray(data)) {
-    return data.map(cleanSingle);
-  } else {
-    return cleanSingle(data);
-  }
-}
+// function cleanData(data) {
+//   if (Array.isArray(data)) {
+//     return data.map(cleanSingle);
+//   } else {
+//     return cleanSingle(data);
+//   }
+// }
 
-function main() {
-  let input = '';
+// function main() {
+//   let input = '';
 
-  process.stdin.setEncoding('utf8');
-  process.stdin.on('data', (chunk) => (input += chunk));
-  process.stdin.on('end', () => {
-    try {
-      const parsed = JSON.parse(input);
-      const cleaned = cleanData(parsed);
-      process.stdout.write(JSON.stringify(cleaned, null, 2));
-    } catch (e) {
-      console.error('⚠️ Error en filtro:', e);
-      process.stdout.write(input);
-    }
-  });
-}
+//   process.stdin.setEncoding('utf8');
+//   process.stdin.on('data', (chunk) => (input += chunk));
+//   process.stdin.on('end', () => {
+//     try {
+//       const parsed = JSON.parse(input);
+//       const cleaned = cleanData(parsed);
+//       process.stdout.write(JSON.stringify(cleaned, null, 2));
+//     } catch (e) {
+//       console.error('⚠️ Error en filtro:', e);
+//       process.stdout.write(input);
+//     }
+//   });
+// }
